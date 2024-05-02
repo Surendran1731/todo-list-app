5c4de922ba849e77a5eaeb44d1e87f3b11b0f8d6
Surendran1731 <143333240+Surendran1731@users.noreply.github.com>
2024-05-02 19:52:06 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <>
+    <footer className="footer fixed-bottom bg-dark text-light p-3">
+    <div className="container bg-dark ">
+      <p className="text-center bg-dark mb-0"><span className="bg-dark ">&copy; 2024 Surendran All rights reserved.</span></p>
+    </div>
+  </footer>
+    </>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css'
+
+function Header() {
+  return (
+   <div className='header'>
+   <nav className="navbar navbar-dark bg-dark">
+   <div className="container-fluid bg-dark">
+     <span className="navbar-brand mb-0 h1 bg-dark">My ToDo Page 😎</span>
+   </div>
+ </nav></div>
+  )
+}
+
+export default Header
